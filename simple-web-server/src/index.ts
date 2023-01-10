c4bfee359e2242d8b1064a0c4c94f7e057561047
Felix Anducho <felix.anducho74@gmail.com>
2023-01-09 18:21:27 -0600

    Add basic config to connect a postgresql database to server

--- simple-web-server/src/index.ts
+++ simple-web-server/src/index.ts
@@ -1,4 +1,14 @@
 import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+import { Client } from "pg"
+dotenv.config()
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as unknown as number,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
 
 const app: Express = express();
 
@@ -6,6 +16,14 @@ app.get("/", (req: Request, res: Response) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
-app.listen(3000, () => {
-    console.log("[SERVER]: Server is running on port 3000")
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("[SERVER]: Server is running on port: " + process.env.SERVER_PORT)
+})
+
+client.connect((err) => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('[DB]: Server is connected to DATABASE')
+    }
 })
