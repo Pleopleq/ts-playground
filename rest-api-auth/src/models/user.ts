4e0813790427fde81537bf1fe86e63bbbd055701
Felix Anducho <felix.anducho74@gmail.com>
2023-01-16 19:20:14 -0600

    Add user model and logic for compare and encrypt passwords

---
+++ rest-api-auth/src/models/user.ts
@@ -0,0 +1,41 @@
+import { model, Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+type User = {
+  email: string;
+  password: string;
+};
+
+export type UserDocument = User & Document;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+
+  this.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  password: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default model<UserDocument>("User", userSchema);
