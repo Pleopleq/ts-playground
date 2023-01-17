6270016ab93538f349e1a72c34d9cd3f2bcd227c
Felix Anducho <felix.anducho74@gmail.com>
2023-01-16 19:32:30 -0600

    Create boilerplate auth routes, controllers and use it in app

--- rest-api-auth/src/app.ts
+++ rest-api-auth/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.set("logger", morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(authRoutes);
 
 export default app;
---
+++ rest-api-auth/src/controllers/user.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const signUp = (req: Request, res: Response) => {
+  res.send("NOT IMPLEMENTED");
+};
+
+export const logIn = (req: Request, res: Response) => {
+  res.send("NOT IMPLEMENTED");
+};
